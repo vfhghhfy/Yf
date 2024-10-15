1cd89b191e46c5b85470e07d152bc9bfc690a3f8
Mohamedxzxzmm <momexprs888@gmail.com>
2024-10-15 19:40:16 +0300

    تم التحديث

--- plugins/main-DA3M.js
+++ plugins/main-DA3M.js
@@ -1,26 +1,11 @@
 let handler = async m => m.reply(`
 
-≡  *TheFlash-Bot-MD*
+≡  *𝙎𝙖𝙨𝙪𝙠𝙚-Bot-MD*
 
 _GROUP:_
 ─────────────
-▢ *Account Zack*
-https://github.com/araab-zack
-─────────────
-▢ *Account Naruto*
-https://github.com/naru12348
-─────────────
-▢ *SUPPORT_GROUP_(AR🇸🇦)*
-https://chat.whatsapp.com/I16t9p7rBz4KiLww5RI7Io
-─────────────
-▢ *SUPPORT_GROUP_(ES🇪🇸)*
-https://chat.whatsapp.com/B9Ovn2IghiFJCoSnWEgsRn
-─────────────
-▢ *YouTube*
-• https://www.youtube.com/@Zack_247
-─────────────
-▢ *YouTube*
-• https://youtube.com/@user-jl2zt4nc6i?si=_nxA_D4KFhJU4ZZm
+▢ *SUPPORT_GROUP_(AR🇪🇬)*
+https://chat.whatsapp.com/IAEHfANHLFU6QZW5WcF9GT
 `.trim())
 handler.help = ['gpflash']
 handler.tags = ['main']
