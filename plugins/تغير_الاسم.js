06a28647fecd4e6f42536971601cf939b6c19663
Mohamedxzxzmm <momexprs888@gmail.com>
2024-10-15 20:47:10 +0300

    تم التحديث

--- plugins/تغير_الاسم.js
+++ plugins/تغير_الاسم.js
@@ -1,12 +1,12 @@
 let handler  = async (m, { conn, args, text }) => {
-if (!text) throw `*[❗اشعار❗] مثال : .تغير-الاسم 𝐅𝐋𝐀𝐒𝐇 ⚡ 𝐁𝐎𝐓*`
+if (!text) throw `*[❗اشعار❗] مثال : .تغير-الاسم 𝙎𝙖𝙨𝙪𝙠𝙚 ⚡ 𝐁𝐎𝐓*`
 try {
 let text = args.join` `
 if(!args || !args[0]) {
 } else {
 conn.groupUpdateSubject(m.chat, text)}
 } catch (e) {
-throw '*[❗اشعار❗] *مثال : .تغيرالاسم 𝐅𝐋𝐀𝐒𝐇 ⚡ 𝐁𝐎𝐓*'
+throw '*[❗اشعار❗] *مثال : .تغيرالاسم 𝙎𝙖𝙨𝙪𝙠𝙚 ⚡ 𝐁𝐎𝐓*'
 }}
 handler.help = ['setname <text>']
 handler.tags = ['group']
