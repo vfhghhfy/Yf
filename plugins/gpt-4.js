615b6a6a7fc1fe4b3041824a5725030421a57ab2
Mohamedxzxzmm <momexprs888@gmail.com>
2024-10-15 19:26:34 +0300

    تم التحديث

--- plugins/gpt-4.js
+++ plugins/gpt-4.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, text, args, usedPrefix, command }) => {
       'https://envs.sh/wHc.jpg', 
       [['الــمــطـور', `.المطور`]], 
       null, 
-      [['قــنـاة الــبــوت', `https://whatsapp.com/channel/0029ValRCUIFi8xmK9xBOr0I`]], 
+      [['قــنـاة الــبــوت', `https://whatsapp.com/channel/0029VaklBGFHFxOwODjsoP13`]], 
       m
     );
   } catch (error) {
