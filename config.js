63cf94939e0936a1c0a7a395f1c3f19f2d3a3881
vfhghhfy <tt8211099@gmail.com>
2025-09-25 15:49:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["201229466261", '👑 𝙎𝙖𝙨𝙪𝙠𝙚 - MOHAMED 👑', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["967778668253", '👑 𝙎𝙖𝙨𝙪𝙠𝙚 - MOHAMED 👑', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ]
 
 
